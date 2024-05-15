9ec906537089cd7eb3cf4b2a82ab71a6638cdfe4
albadelesp <albadelesp@gmail.com>
2024-05-15 11:34:23 +0200

    añadidos nombres de usuarios en el chat

--- screens/EditProfileScreen.tsx
+++ screens/EditProfileScreen.tsx
@@ -2,53 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, ScrollView, Text, View, Alert, TextInput } from 'react-native';
 import { Button } from 'react-native-elements';
 import { getAuth } from 'firebase/auth';
-import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { StackScreenProps } from '@react-navigation/stack';
-import ProfileScreen from './ProfileScreen';
-
 
 const auth = getAuth();
 
 const EditProfileScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
   const [profileData, setProfileData] = useState({
-    name: '',
     laboralExperience: '',
     previousJobs: '',
     education: '',
   });
 
   const saveProfileData = async () => {
-    if (!profileData.name) {
-      Alert.alert('Error', 'Por favor, introduce tu nombre y apellidos.');
-      return;
-    }
-    const handleSaveProfile = async () => {
-      try {
-        if (auth.currentUser) {
-          const userId = auth.currentUser.uid;
-          const profileDocRef = doc(db, 'users', userId);
-          await updateDoc(profileDocRef, {
-            name: profileData.name,
-            laboralExperience: profileData.laboralExperience,
-            previousJobs: profileData.previousJobs,
-            education: profileData.education,
-          });
-          console.log('Perfil actualizado correctamente');
-        } else {
-          console.log('El usuario no está autenticado');
-        }
-      } catch (error) {
-        console.error('Error al guardar perfil:', error);
-      }
-    };
-
     const userUUID = auth.currentUser?.uid;
     if (userUUID) {
       const profileRef = doc(db, 'users', userUUID);
       try {
         await setDoc(profileRef, {
-          name: profileData.name,
+          // Remove name update to keep it unchangeable
           laboralExperience: profileData.laboralExperience,
           previousJobs: profileData.previousJobs,
           education: profileData.education,
@@ -61,7 +34,7 @@ const EditProfileScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
       }
     }
   };
-
+  
   useEffect(() => {
     const fetchProfileData = async () => {
       const userUUID = auth.currentUser?.uid;
@@ -72,7 +45,6 @@ const EditProfileScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
           if (profileSnapshot.exists()) {
             const data = profileSnapshot.data();
             setProfileData({
-              name: data.name || '',
               laboralExperience: data.laboralExperience || '',
               previousJobs: data.previousJobs || '',
               education: data.education || '',
@@ -92,12 +64,6 @@ const EditProfileScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.formContainer}>
           <Text style={styles.title}>Edita tus datos</Text>
-          <Text style={[styles.label, styles.nameLabel]}>Nombre y apellidos *</Text>
-          <TextInput
-            value={profileData.name}
-            onChangeText={(text) => setProfileData({ ...profileData, name: text })}
-            style={[styles.textInput, styles.smallTextInput]}
-          />
           <Text style={styles.label}>Experiencia Laboral</Text>
           <TextInput
             value={profileData.laboralExperience}
@@ -135,6 +101,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingTop: 20,
   },
   scrollView: {
     flexGrow: 1,
@@ -153,18 +121,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginBottom: 10,
   },
-  nameLabel: {
-    marginBottom: 5,
-  },
   textInput: {
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
     padding: 10,
     marginBottom: 20,
-  },
-  smallTextInput: {
-    height: 40,
   },
   largeTextInput: {
     minHeight: 120,
